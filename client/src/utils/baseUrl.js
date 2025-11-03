edfad900a39718019c3c7ca65fb4c5a62132fc7b
Manish <manishotusone@gmail.com>
2025-11-03 11:45:35 +0530

    Url Change

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost:8001/api/v1.0";
+// export const baseUrl = "http://localhost:8001/api/v1.0";
+export const baseUrl = "https://saf-i8nk.onrender.com/api/v1.0";
