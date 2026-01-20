d739df3c70a91acc7c8defd9e81d670ace8abca0
Manish <manishotusone@gmail.com>
2026-01-20 10:32:48 +0530

    Port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 // ✅ Start server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
